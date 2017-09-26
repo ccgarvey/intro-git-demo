d1d25a72b7765fbda53e94faa44a65a336bd7b10
Clarissa Garvey <ccgarve@emory.edu>
2017-09-26 18:18:35 -0400

    Add error checking to fibonacci

--- fibonacci.m
+++ fibonacci.m
@@ -1,5 +1,8 @@
 function value = fibonacci(index)
     % Returns the value at a given index (1-indexed) in the Fibonacci sequence.
+    if index < 1
+        error(sprintf('Fibonacci requires positive index, got %d', index))
+    end
     
     previous = 0;
     current = 1;
